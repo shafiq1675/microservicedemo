b1f315646ca1752512bd5fadea1fa3d6d510d8e8
shafiq1675 <islam.mdshafiq@gmail.com>
2024-10-06 23:54:32 +0600

    ss

---
+++ src/Services/Catelogue.API/AppHostedServices/AppHostedServices.cs
@@ -0,0 +1,19 @@
+﻿
+using Catelogue.API.Context;
+
+namespace Catelogue.API.AppHostedServices
+{
+    public class AppHostedServices : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            CatelogueDBContextSeed.Seed();
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- src/Services/Catelogue.API/Models/Product.cs
+++ src/Services/Catelogue.API/Models/Product.cs
@@ -8,8 +8,8 @@ namespace Catelogue.API.Models
         public string Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string category { get; set; }
-        public string Summery { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
         public string ImageFile { get; set; }
         public decimal Price { get; set; }
     }
